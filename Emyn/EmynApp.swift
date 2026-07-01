0c5f109d06a0e89988d2dff72f330fc058942994
Benedikt Terhechte <terhechte@gmail.com>
2026-07-01 09:28:36 +0200

    Initial Commit

---
+++ Emyn/EmynApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmynApp.swift
+//  Emyn
+//
+//  Created by Benedikt Terhechte on 01.07.26.
+//
+
+import SwiftUI
+
+@main
+struct EmynApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
